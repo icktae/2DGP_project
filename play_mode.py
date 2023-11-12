8dd0f41de267f5544dae4f9b275e1a4a33fb20ab
icktae <taechil77@naver.com>
2023-11-12 21:09:45 +0900

    save

--- play_mode.py
+++ play_mode.py
@@ -35,23 +35,23 @@ def init():
     boy = Boy()
     game_world.add_object(boy, 1)
 
-    zombies = [Zombie() for _ in range(5)]
-    game_world.add_objects(zombies, 1)
-    game_world.add_collision_pair('boy:zombie', boy, None)
-    for zombie in zombies:
-        game_world.add_collision_pair('boy:zombie', None, zombie)
-        game_world.add_collision_pair('zombie:ball', zombie, None)
+    #zombies = [Zombie() for _ in range(5)]
+    #game_world.add_objects(zombies, 1)
+    #game_world.add_collision_pair('boy:zombie', boy, None)
+    #for zombie in zombies:
+     #   game_world.add_collision_pair('boy:zombie', None, zombie)
+      #  game_world.add_collision_pair('zombie:ball', zombie, None)
 
     # fill here
-    global balls
-    balls = [Ball(random.randint(100, 1600 - 100), 60, 0) for _ in range(30)]
-    game_world.add_objects(balls, 1)
-
-
-
-    game_world.add_collision_pair('boy:ball', boy, None)
-    for ball in balls:
-        game_world.add_collision_pair('boy:ball', None, ball)
+    # global balls
+    # balls = [Ball(random.randint(100, 1600 - 100), 60, 0) for _ in range(30)]
+    # game_world.add_objects(balls, 1)
+    #
+    #
+    #
+    # game_world.add_collision_pair('boy:ball', boy, None)
+    # for ball in balls:
+    #     game_world.add_collision_pair('boy:ball', None, ball)
 
 
 
@@ -76,4 +76,3 @@ def pause():
 
 def resume():
     pass
-
